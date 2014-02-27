b8cfa08871f013418799503b43b56b4f5b84d8c9
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-02-27 17:21:24 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoteBook::Application.config.secret_key_base = '09302e9c9c1cc02909db015d0dfea98e7f91dfbb8ba564928ffecbc70b5bc52bc9f74e6e6ae578fc2b5bc0cb059ab02e246cd1ebe19f79ce8e755431bccfee72'
