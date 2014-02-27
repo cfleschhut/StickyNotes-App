18737c51078faea0018bb77ece383039adddd66a
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-02-27 18:15:16 +0100

    note model + seed data

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ NoteBook::Application.routes.draw do
 
   root 'main#index'
 
+  scope 'api' do
+    resources :notes
+  end
+
+  get '*path' => 'main#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
